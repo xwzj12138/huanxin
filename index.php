fe5d4fb358f96783da47b23ee5aa1bfaecc1cc09
packer-陈星星 <13246190524@163.com>
2019-10-09 10:21:52 +0800

    Signed-off-by: packer-陈星星 <13246190524@163.com>

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/9
+ * Time: 10:00
+ */
+
+require_once "vendor/autoload.php";
+
+$huanxin = new \Huanxin\Token('juworker','jiapin');
+echo $huanxin->getToken('YXA60qhoi-caQtaShFCW38oMvw','YXA6k94f8G_PN2xHVcWEtNLAT8z-ufk');
